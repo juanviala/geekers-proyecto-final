8a9c72c9beb8167a1f22fa161a34a078454c09a0
juanviala <67595371+juanviala@users.noreply.github.com>
2021-02-24 17:26:03 -0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,7 +7,7 @@ const Item = ({ productsJson }) => {
                 {productsJson.map(product => 
                     <div className="col-xs-3 col-md-3 mb-3" key={product.id}>
                         <Link to={`/item/${product.id}`} style={{color:'black'}}>
-                            <img className="card-img-top" src={`../public/images/products/${product.image}`} alt={product.name} />
+                            <img className="card-img-top" src={`../../public/images/products/${product.image}`} alt={product.name} />
                             <h5>{product.name}</h5>
                             <h6>${product.price}</h6>
                         </Link>
